079b78e3c29fa8301ae7b7523cb34f44aaf91d15
Tyler Wong <tylerwong.j@gmail.com>
2020-05-20 19:04:08 -0700

    master - next.js init page - works on node 10+ "nvm use 10"; nextJS sample project; bring in nextJS .gitignore file

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar'
+
+const About = () => (
+	<div>
+		<Navbar />
+		<h1>About Weather Tracker API</h1>
+	</div>
+);
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Navbar from '../components/Navbar'
+
+const Index = () => (
+	<div>
+		<Navbar />
+		<h1>Weather Tracker API</h1>
+	</div>
+)
+
+export default Index;
